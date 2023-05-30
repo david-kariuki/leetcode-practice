db1d9489ad3a1ff6069b99e5389fa98181008881
David Kariuki <14153276+david-kariuki@users.noreply.github.com>
2023-05-30 06:47:25 +0300

    Leaning arrays
    Signed-off-by: David Kariuki <14153276+david-kariuki@users.noreply.github.com>

---
+++ src/main/java/dk/learning/learn_dsa/array_and_string/questions/PlusOne.java
@@ -0,0 +1,32 @@
+/**
+ * @apiNote PlusOne class
+ * @author David Kariuki
+ * @version 1.0.0
+ * @since 5/22/23 @Question Find Pivot Index
+ *     <p>You are given a large integer represented as an integer array digits, where each digits[i]
+ *     is the ith digit of the integer. The digits are ordered from most significant to least
+ *     significant in left-to-right order.
+ *     <p>The large integer does not contain any leading 0's. Increment the large integer by one and
+ *     return the resulting array of digits.
+ */
+package dk.learning.learn_dsa.array_and_string.questions;
+
+public class PlusOne {
+
+  public static void main(String[] args) {
+
+    int[] digits = new int[] {1, 2, 3};
+    System.out.println(plusOne(digits)); // Output: [1,2,4]
+
+    digits = new int[] {4, 3, 2, 1};
+    System.out.println(plusOne(digits)); // Output: [4,3,2,2]
+
+    digits = new int[] {9};
+    System.out.println(plusOne(digits)); // Output: [1,0]
+  }
+
+  public static int plusOne(int[] digits) {
+
+    return -1;
+  }
+}
