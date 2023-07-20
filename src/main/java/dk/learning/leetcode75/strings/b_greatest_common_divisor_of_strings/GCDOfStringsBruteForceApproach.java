d6491594e1c975e52ceea9b26abb9cf19d57ed14
David Kariuki <14153276+david-kariuki@users.noreply.github.com>
2023-07-20 20:38:58 +0300

    refactor: Refactor
    Signed-off-by: David Kariuki <14153276+david-kariuki@users.noreply.github.com>

--- src/main/java/dk/learning/leetcode75/strings/b_greatest_common_divisor_of_strings/GCDOfStringsBruteForceApproach.java
+++ src/main/java/dk/learning/leetcode75/strings/b_greatest_common_divisor_of_strings/GCDOfStringsBruteForceApproach.java
@@ -12,20 +12,6 @@
  * @constraints Constraints
  *     <p>1 <= str1.length, str2.length <= 1000</
  *     <p>str1 and str2 consist of English uppercase letters.
- * @complexityAnalysis Complexity analysis
- *     <p>Let m,nm, nm,n be the lengths of the two input strings str1 and str2.
- * @timecomplexity Time Complexity
- *     <p>O(min(m,n)⋅(m+n))O(\min(m, n) \cdot (m + n))O(min(m,n)⋅(m+n)) We checked every prefix
- *     string base of the shorter string among str1 and str2, and verify if both strings are made by
- *     multiples of base.
- *     <p>There are up to min(m,n)\min(m, n)min(m,n) prefix strings to verify and each check
- *     involves iterating over the two input strings to check if the current base is the GCD string,
- *     which costs O(m+n)O(m + n)O(m+n).
- *     <p>Therefore, the overall time complexity is O(min(m,n)⋅(m+n))O(\min(m, n) \cdot (m +
- *     n))O(min(m,n)⋅(m+n)).
- * @spacecomplexity Space Complexity
- *     <p>O(min(m,n))O(\min(m, n))O(min(m,n)) We need to keep a copy of base in each iteration,
- *     which takes O(min(m,n))O(\min(m, n))O(min(m,n)) space.
  * @since 02/07/23
  * @see <a
  *     href="https://leetcode.com/problems/greatest-common-divisor-of-strings/editorial/?envType=study-plan-v2&envId=leetcode-75">
@@ -39,8 +25,7 @@
  *     the two input strings to check if the current base is the GCD string, which costs O( m + n).
  *     <p>Therefore, the overall time complexity is O(min(m,n)⋅(m+n)).
  * @spacecomplexity Space complexity: O(min(m,n))
- *     <p>We need to keep a copy of base in each iteration, which takes O(min⁡(m,n))O(\min(m,
- *     n))O(min(m,n)) space.
+ *     <p>We need to keep a copy of base in each iteration, which takes O(min(m,n)) space.
  */
 package dk.learning.leetcode75.strings.b_greatest_common_divisor_of_strings;
 
