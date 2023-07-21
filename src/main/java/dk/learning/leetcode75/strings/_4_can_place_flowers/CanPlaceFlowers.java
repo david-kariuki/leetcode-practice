a689d4220bcba2ea0b2e3085011e72eb7c93e76e
David Kariuki <14153276+david-kariuki@users.noreply.github.com>
2023-07-21 06:27:36 +0300

    chore: LeetCode75
    Signed-off-by: David Kariuki <14153276+david-kariuki@users.noreply.github.com>

--- src/main/java/dk/learning/leetcode75/strings/_4_can_place_flowers/CanPlaceFlowers.java
+++ src/main/java/dk/learning/leetcode75/strings/_4_can_place_flowers/CanPlaceFlowers.java
@@ -7,10 +7,18 @@
  *     <p>Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means
  *     not empty, and an integer n, return true if n new flowers can be planted in the flowerbed
  *     without violating the no-adjacent-flowers rule and false otherwise.
- * @example Example 1: Input: flowerbed = [1,0,0,0,1], n = 1 Output: true
- * @example Example 2: Input: flowerbed = [1,0,0,0,1], n = 2 Output: false
- * @constraints 1 <= flowerbed.length <= 2 * 104 flowerbed[i] is 0 or 1. There are no two adjacent
- *     flowers in flowerbed. 0 <= n <= flowerbed.length
+ * @example Example 1:
+ *     <p>Input: flowerbed = [1,0,0,0,1], n = 1
+ *     <p>Output: true
+ * @example Example 2:
+ *     <p>Input: flowerbed = [1,0,0,0,1], n = 2
+ *     <p>Output: false
+ * @constraints
+ *     <p>1 <= flowerbed.length <= 2 *
+ *     <p>104
+ *     <p>flowerbed[i] is 0 or 1.
+ *     <p>There are no two adjacent flowers in flowerbed.
+ *     <p>0 <= n <= flowerbed.length
  * @since 20/07/23
  * @see <a
  *     href="https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75">
@@ -20,5 +28,47 @@ package dk.learning.leetcode75.strings._4_can_place_flowers;
 
 public class CanPlaceFlowers {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+
+    // Example 1:
+    int n = 1;
+    int[] flowerbed = {1, 0, 0, 0, 1};
+    System.out.printf(
+        "\n\n%s flower(s) planted without violating adjacent rule %s",
+        n, canPlaceFlowers(flowerbed, n));
+
+    // Example 2:
+    n = 2;
+    flowerbed = new int[] {1, 0, 0, 0, 1};
+    System.out.printf(
+        "\n\n%s flower(s) planted without violating adjacent rule %s",
+        n, canPlaceFlowers(flowerbed, n));
+
+    // Example 3:
+    n = 2;
+    flowerbed = new int[] {1, 0, 0, 0, 0, 0, 0, 0, 1};
+    System.out.printf(
+            "\n\n%s flower(s) planted without violating adjacent rule %s",
+            n, canPlaceFlowers(flowerbed, n));
+  }
+
+  public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+    for (int i = 0; i < flowerbed.length; i++) {
+      if (n == 0) return true; // Check planting count
+      if (flowerbed[i] == 1) continue; // Skip if current position planted
+
+      if (i == 0 && flowerbed[i + 1] == 0) {
+        flowerbed[i] = 1;
+        continue;
+      }
+
+      if ((flowerbed[i] == 0) && (flowerbed[i - 1] == 0) && (flowerbed[i + 1] == 0)) {
+        flowerbed[i] = 1;
+        n--;
+      }
+    }
+
+    return false;
+  }
 }
